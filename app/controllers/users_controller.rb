1f789691e32939445a16c39890ef69a61a7c136d
kabin322 <kabin322@gmail.com>
2016-02-02 07:58:35 +0000

    add profile & location

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
 
-  def show # 追加
+  def show
     @user = User.find(params[:id])
   end
 
@@ -12,15 +13,32 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user #ここを修正
+      redirect_to @user
     else
       render 'new'
+    end
+  end
+  
+  def edit
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "プロフィールを編集しました"
+      redirect_to @user
+    else
+      render 'edit'
     end
   end
   
   private
   
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :profile, :location)
+  end
+  
+  def set_user
+    @user = User.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+
   root to: 'static_pages#home'
   get 'signup',  to: 'users#new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  get 'edit', to:'users#edit'
+  post 'edit', to:'users#update'
+  
 
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 end
